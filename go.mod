d4acb42d07e2f1d5ccd527b117ed8399aee5f367
nqvinh00 <nqvinh00@gmail.com>
2024-02-27 20:48:31 +0700

    init code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nqvinh00/CakeAssignment
+
+go 1.21.5
---
+++ model/config.go
@@ -0,0 +1,20 @@
+package model
+
+type Config struct {
+}
+
+type HTTP struct {
+	Host string `yaml:"host"`
+	Port int    `yaml:"port"`
+}
+
+type Database struct {
+	DB              string `yaml:"db"`
+	Username        string `yaml:"username"`
+	Password        string `yaml:"password"`
+	Host            string `yaml:"host"`
+	Args            string `yaml:"args"`
+	MaxOpenConns    int    `yaml:"max_open_conns"`
+	MaxIdleConns    int    `yaml:"max_idel_conns"`
+	MaxConnLifeTime int    `yaml:"max_conn_life_time"`
+}
